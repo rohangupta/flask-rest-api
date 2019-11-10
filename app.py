8b3e07cf1326e0f3f136eaaaeb5345e7bc5b275c
Rohan Gupta <rohangupta1994@gmail.com>
2019-11-10 18:10:55 -0500

    Added Feedback API

--- app.py
+++ app.py
@@ -6,16 +6,29 @@ api = Api(app)
 items = list()
 
 
-class DemoOCR(Resource):
+class Title(Resource):
     def post(self):
-        # POST Request example: http://127.0.0.1:5000/extract_vin
-        # request JSON example: {'image_url': 'https://tinyurl.com/y4umqx6t'}
+        # REST Endpoint: http://127.0.0.1:5000/extract_vin
+        # Request JSON example: {'image_url': 'https://tinyurl.com/y4umqx6t'}
+        # Response JSON example: {'vin': "1VWBT7A30EC033777"}
         data = request.get_json()
         if 'image_url' not in data or data['image_url'].strip() == "":
-            return {'vin': None}, 404
+            return {'vin': 'image url not found'}, 404
         return {'vin': "1VWBT7A30EC033777"}
 
-api.add_resource(DemoOCR, '/extract_vin')
+class Feedback(Resource):
+    def post(self):
+        # REST Endpoint: http://127.0.0.1:5000/save_feedback
+        # Request JSON example: {'image_url': 'https://tinyurl.com/y4umqx6t', 'feedback': 'yes'}
+        # Response JSON example: {}
+        data = request.get_json()
+        if 'image_url' not in data or data['image_url'].strip() == "":
+            return {}, 404
+        print('feedback: '+data['feedback'])
+        return {}
+
+api.add_resource(Title, '/extract_vin')
+api.add_resource(Feedback, '/save_feedback')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
